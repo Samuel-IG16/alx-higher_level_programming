cb31747330ede41da7d4f963bc7eced4054ddbf7
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-06-28 21:07:17 -0700

    Task 1 completed!

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {number % 10}", end=" ")
+print(f"Last digit of {number} is {abs(number) % 10}", end=" ")
 if (number % 10) > 5:
     print("and is greater than 5")
 if (number % 10) == 0:
