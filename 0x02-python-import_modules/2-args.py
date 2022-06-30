92f4822d435d6ded440e693a8d378a0e3ef69362
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-06-30 16:48:16 -0700

    Task 2 completed!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments: ".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("{} argument: ".format(len(argv) - 1))
+        else:
+            print("{} arguments: ".format(len(argv) - 1))
         for item in argv:
             if argv.index(item) == 0:
                 continue
