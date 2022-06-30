a97bc8c06bf571e9517490fb2c532f387651a160
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-06-30 08:41:35 -0700

    Task 1 completed!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,3 +6,6 @@ print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
