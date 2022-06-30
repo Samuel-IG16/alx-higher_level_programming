fb82c365c58c5a398293914b1e993bcd812864bd
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-06-30 09:28:26 -0700

    Task 0 completed!

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-
 a = 1
 b = 2
-
 if __name__ == "__main__":
-    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
