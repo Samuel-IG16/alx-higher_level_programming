7ac2fbd30954ea6a03aafac398132cfb88635a2f
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-06-30 17:13:15 -0700

    Task 3 completed!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 summ = 0
-for nums in argv:
-    if argv.index(nums) == 0:
-        continue
-    summ += eval(nums)
-print(summ)
+if __name__ == "__main__":
+    for nums in argv:
+        if argv.index(nums) == 0:
+            continue
+        summ += int(nums)
+    print(summ)
