4461d069a81ebedd160a92dbae4b9cfc86f3d798
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-04 15:04:35 -0700

    Task 13 completed!

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -14,14 +15,15 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
