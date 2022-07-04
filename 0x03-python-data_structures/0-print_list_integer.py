a8aaaae83ef853ad50f90a916577e43e3be11011
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-04 14:53:54 -0700

    Task 0 completed!

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for numm in my_list:
-        print("{:d}".format(numm))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
