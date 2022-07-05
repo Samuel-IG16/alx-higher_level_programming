3d6429ebb532405ccd048a2dc357d87661b32b58
Igbinijesu Samuel <65024899+Samuel-IG16@users.noreply.github.com>
2022-07-06 00:08:52 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
+from calculator_1 import add, sub, mul, div
 
+ops = ['+', '-', '*', '/']
+argc = len(argv)
 
-def main():
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        return 1
+if __name__ == '__main__':
+    if (argc - 1) < 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    elif argv[2] not in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
     else:
-        a = argv[1]
-        op = argv[2]
-        b = argv[3]
-        if op == "+":
-            print("{} + {} = {}".format(a, b, add(int(a), int(b))))
-        elif op == "-":
-            print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
-        elif op == "*":
-            print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
-        elif op == "/":
-            print("{} / {} = {}".format(a, b, div(int(a), int(b))))
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            return 1
-
-
-if __name__ == "__main__":
-    main()
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
