4ceb2490f0c3b7aa6e7bf7ef869aaa84879e31c4
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-06 17:15:38 -0700

    Task 6 completed!

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        for i in a_dictionary:
-            if i == key:
-                a_dictionary[i] = value
-    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary):
+        print("{:s}: {}".format(i, a_dictionary[i]))
