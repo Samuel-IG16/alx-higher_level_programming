003ce3eca7b8ceaab6079d53e2d5c3f9ce6a3075
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-27 17:05:30 -0700

    Task 101 completed!

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -19,12 +19,14 @@ if __name__ == "__main__":
     # initialize the answer list
     for i in range(n):
         a.append([i, None])
+
     def already_exists(y):
         """check that a queen does not already exist in that y value"""
         for x in range(n):
             if y == a[x][1]:
                 return True
         return False
+
     def reject(x, y):
         """determines whether or not to reject the solution"""
         if (already_exists(y)):
@@ -35,10 +37,12 @@ if __name__ == "__main__":
                 return False
             i += 1
         return True
+
     def clear_a(x):
         """clears the answers from the point of failure on"""
         for i in range(x, n):
             a[i][1] = None
+
     def nqueens(x):
         """recursive backtracking function to find the solution"""
         for y in range(n):
