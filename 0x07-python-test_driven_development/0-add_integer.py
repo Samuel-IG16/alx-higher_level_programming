44231910cffefbf724d7259c01a332b094857619
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-27 17:09:11 -0700

    Tasks

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for add_integer method"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: first int.
+	b: second int, default value is 98.
+
+    Raises:
+        TypeError: if a, b are neither int nor float.
+
+    Returns:
+        sum of a and b.
+    """
+
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for matrix_divided method"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix.
+
+    Args:
+        matrix: The matrix whoses elements are to be divided by div.
+	div: The dividing number.
+
+    Raises:
+        TypeError: if matrix is not a list of lists of int or float.
+	TypeError: if each row of matrix is not of same size.
+	TypeError: if div is neither an int nor float
+	ZeroDivisionError: if div is zero
+
+    Returns:
+        a new matrix with elements rounded to 2 decimal places.
+    """
+
+
+    
+
+    if __name___=="__main__":
+        import doctest
+	doctest.testfile("tests/2-matrix_divided.txt")
