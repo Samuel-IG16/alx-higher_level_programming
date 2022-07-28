d1869fabbd03047a20d25ef835abbcae4e888c91
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-27 17:41:08 -0700

    Task 2 completed!

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,21 +7,18 @@ def matrix_divided(matrix, div):
 
     Args:
         matrix: The matrix whoses elements are to be divided by div.
-	div: The dividing number.
+        div: The dividing number.
 
     Raises:
         TypeError: if matrix is not a list of lists of int or float.
-	TypeError: if each row of matrix is not of same size.
-	TypeError: if div is neither an int nor float
-	ZeroDivisionError: if div is zero
+        TypeError: if each row of matrix is not of same size.
+        TypeError: if div is neither an int nor float
+        ZeroDivisionError: if div is zero
 
     Returns:
         a new matrix with elements rounded to 2 decimal places.
     """
 
-
-    
-
-    if __name___=="__main__":
+    if __name___ == "__main__":
         import doctest
-	doctest.testfile("tests/2-matrix_divided.txt")
+        doctest.testfile("tests/2-matrix_divided.txt")
