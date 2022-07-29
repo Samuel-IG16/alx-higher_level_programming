6f48b6eb4e19e6a9b53b43fcfbe976dc57edf1b2
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-29 20:39:49 +0100

    Task 12 completed!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz ", end="")
         elif i % 5 == 0:
             print("Buzz ", end="")
-        elif i % 15 == 0:
+        elif i % 3 and i % 5 == 0:
             print("FizzBuzz ", end="")
         else:
             print("{:d} ".format(i), end="")
