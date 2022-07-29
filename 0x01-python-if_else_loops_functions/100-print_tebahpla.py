3c513eeb275438d332cd0b2d020647c2116bd7da
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-07-29 20:58:37 +0100

    Task 100 completed!

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-		ascii_num = ascii_num - 32
-	print("{:c}".format(ascii_num), end='')
+    if ascii_num % 2 == 1:
+        ascii_num = ascii_num - 32
+    print("{:c}".format(ascii_num), end='')
