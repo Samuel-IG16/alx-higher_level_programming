bf783eda42ddb35f7b255930f2b10a8e9b87d6ba
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-03 00:47:04 +0100

    Update and rename 5-to_json_string.py to 5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 5-save_to_json_file.
+Writes an Object to a text file,
+using a JSON representation.
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes the representation of my_obj
+    to filename.
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
+
+    with open(filename, 'w+') as f:
+        json.dump(my_obj, f)
