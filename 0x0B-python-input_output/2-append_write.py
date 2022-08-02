1f9723f37454c12b422ae5df32ab39069e89c37b
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-03 00:39:19 +0100

    Update and rename 2-read_lines.py to 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module 4-append_write.
+Appends a string at the end of a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to filename.
+    Args:
+        - filename: name of the file
+        - text: text to append
+    Returns: the number of characters added
+    """
+
+    with open(filename, 'a+') as f:
+        return f.write(text)
