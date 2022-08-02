1a6e6982e0187e65ca6774947425aef4ee33123e
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-03 00:54:05 +0100

    Update and rename 8-load_from_json_file.py to 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 8-class_to_json.
+Returns the dictionary description with
+simple data structure (list, dictionary,
+string, integer and boolean)
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """Creates a dict description of obj.
+    Args:
+        - obj: object to serialize
+    Returns: dictionnary description of obj
+    """
+
+    return obj.
