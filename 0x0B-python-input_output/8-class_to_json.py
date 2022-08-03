ab67b7b1388c68c20f9e19bc33e91ff4f8561eb2
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-03 01:03:41 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,9 +9,11 @@ for JSON serialization of an object.
 
 def class_to_json(obj):
     """Creates a dict description of obj.
+
     Args:
         - obj: object to serialize
+
     Returns: dictionnary description of obj
     """
 
-    return obj.
+    return obj.__dict__
