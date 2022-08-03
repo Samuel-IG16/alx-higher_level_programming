a6ac3d521286b13ce8c003b2624bb3ccd1dd3c00
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-03 01:16:41 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -24,23 +24,22 @@ if __name__ == "__main__":
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
-  
+
     try:
         for line in sys.stdin:
             if count == 10:
                 print_stats(size, status_codes)
                 count = 1
-                
             else:
                 count += 1
-             
+
             line = line.split()
-          
+
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
-           
+
             try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
@@ -49,9 +48,9 @@ if __name__ == "__main__":
                         status_codes[line[-2]] += 1
             except IndexError:
                 pass
-           
+
         print_stats(size, status_codes)
-    
+
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
