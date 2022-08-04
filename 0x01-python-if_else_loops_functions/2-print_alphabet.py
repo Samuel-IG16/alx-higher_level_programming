936092861c1e44d55461d73713ce80c8eda5aec4
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:05:36 +0100

    Task 2 completed!

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{0}".format(chr(i)), end='')
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the ASCII alphabet in lowercase
+# demonstrates how to use a loop to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# For each ascii_code in range sequence
+for ascii_code in range(97, 123):
+
+    # Print out the character format of the ascii_code
+    print(f"{chr(ascii_code)}", end='')
