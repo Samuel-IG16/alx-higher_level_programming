673364f1a32ffb2a5f263ba3f0622a78f50c92d5
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:08:04 +0100

    Task 12 completed!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the numbers from 1 to 100 separated by a space
+# demonstrates how to use a function, a loop and an if...elif condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 != 0:
-            print("Fizz ", end="")
-        elif i % 5 == 0 and i % 3 != 0:
-            print("Buzz ", end="")
-        elif i % 15 == 0:
-            print("FizzBuzz ", end="")
-        else:
-            print("{:d} ".format(i), end="")
+  """Print out numbers from 1 to 100"""
+
+  # For number in range sequence
+  for number in range(1, 101):
+
+    # If number is a multiple of three
+    if number % 3 == 0 and number % 5 != 0:
+
+      # Print out "Fizz "
+      print("Fizz ", end="")
+
+    # If number is a multiple of five but not of three
+    elif number % 5 == 0 and number % 3 != 0:
+
+      # Print out "Buzz "
+      print("Buzz ", end="")
+
+    # If number is a multiple of both three and five
+    elif number % 15 == 0:
+      
+      # Print out "FizzBuzz "
+      print("FizzBuzz ", end="")
+  
+    else:
+      # Print number
+      print(f"{number} ", end="")
