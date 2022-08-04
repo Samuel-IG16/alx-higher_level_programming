1d3217cb2769de1426a2014967d526eafe7c10e2
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:05:06 +0100

    Task 0 completed!

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to determine if a number stored in a variable is positive or negative
+# demonstrates how to use if...elif conditions to determine the program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Generate random numbers and assign it to a variable named number
 import random
 number = random.randint(-10, 10)
 
-# To check if the number is positive or negative
+# Print out the value of number followed by ...
+# ..." is positive", if number is greater than zero
 if number > 0:
-    print(f"{number} is positive")
+  print(f"{number} is positive")
+
+# ..." is negative", if number is less than zero
 elif number < 0:
-    print(f"{number} is negative")
+  print(f"{number} is negative")
+
+# ..." is zero", if number is equals to zero
 else:
-    print(f"{number} is zero")
+  print(f"{number} is zero")
