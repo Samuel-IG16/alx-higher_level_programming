1b4c01b3746349256c609e6bb02541b57122dd62
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:06:41 +0100

    Task 6 completed!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(0, 10):
-        if j <= i:
-            continue
-        else:
-            print("{:d}{:d}".format(i, j), end=", ")
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the smallest, unique combinations of two digits
+# demonstrates how to use loops and an if condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Digits are 0 ... 9
+# Loop variables to increment:  first_digit, second_digit
+# For smallest combination first_digit < second_digit
+# For unique combination first_digit != second_digit 
+# For smallest unique combination !(second_digit <= first_digit)
+
+for first_digit in range(0, 8):
+  for second_digit in range(0, 10):
+    if not (second_digit <= first_digit):
+      print(f"{first_digit}{second_digit}", end=", ")
+
+# Print last values for first_digit and second_digit
 print("89")
