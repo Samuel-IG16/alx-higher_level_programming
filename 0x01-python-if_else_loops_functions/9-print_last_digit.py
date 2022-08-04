776d4ceacc4e55ef00a513d5d3fb380f3538b6ba
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:07:26 +0100

    Task 9 completed!

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the last digit of a number
+# demonstrates how to use a function to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def print_last_digit(number):
-    number = int(repr(number)[-1])
-    print("{}".format(number), end="")
-    return number
+    """Prints last digit of number
+
+    Args:
+        number: a number
+    
+    Returns:
+        last_digit: the value of the last digit
+    """
+
+    # Get and store last digit of number in a variable named last_digit
+    last_digit = int(repr(number)[-1])
+
+    # Print out and return the value of last_digit
+    print(f"{last_digit}", end="")
+    return last_digit
