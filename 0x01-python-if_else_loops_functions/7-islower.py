8341b6ad203d8db29a05ff6abb5ed5df9fb4f858
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:06:56 +0100

    Task 7 completed!

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to check for lowercase character using ASCII code
+# demonstrates how to use a function and if condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def islower(c):
+    """Checks for lowercase
+
+    Args:
+        c: a single character argument
+    
+    Returns:
+        True: if c is lowercase
+        False: if c is not lowercase
+    """
     if (ord(c) >= 97 and ord(c) <= 122):
         return True
     return False
