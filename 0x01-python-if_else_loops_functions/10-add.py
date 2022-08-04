48a33d38dde676bf1e0a53549bec34ce8ee0fa05
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:07:40 +0100

    Task 10 completed!

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to add two integers and return the result
+# demonstrates how to use a function to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def add(a, b):
+    """Adds two integers
+
+    Args:
+        a: first integer
+        b: second integer
+    
+    Returns:
+        the result of addition
+    """
     return (a + b)
