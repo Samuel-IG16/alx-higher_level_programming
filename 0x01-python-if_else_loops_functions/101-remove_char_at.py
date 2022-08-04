e1924866bea0de98d4841ddef7c5473dc9646722
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:08:40 +0100

    Task 101 completed!

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to create a copy of the string, removing the character at the position n
+# (not the Python way, the “C array index”)
+# demonstrates how to use a function, and an if...elif condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def remove_char_at(str, n):
+    """Removes the char at an index in a given string
+
+    Args:
+        str: string argument
+        n: index argument
+    
+    Returns:
+        str: edited string
+    """
+
+    # If the index is a positive integer
     if n >= 0:
+
+        # Use slicing to ommit the character at index n
         str = str[:n] + str[n + 1:]
+
+    # Return editied string
     return str
