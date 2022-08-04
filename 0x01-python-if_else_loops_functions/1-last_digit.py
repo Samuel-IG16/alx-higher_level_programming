bcb66ec888f693fe15693f75754154963ee221e9
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:05:22 +0100

    Task 1 completed!

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrate how to print the last digit of a number stored in a variable
+# demonstrates how to use if...elif conditions to determine the program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Generate random numbers and assign it to a variable named number
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is", end=" ")
-if (number % 10) > 5:
-    print(f"{number % 10} and is greater than 5")
-if (number % 10) == 0:
-    print(f"{number % 10} and is 0")
-if (number % 10) < 6 and (number % 10) != 0:
-    print(f"-{str(number)[-1]} and is less than 6 and not 0")
+
+# Get and store last digit of number in a variable named last_digit
+last_digit = int(repr(number)[-1])
+
+# Print out number and it's last digit followed by ...
+# ..." and is greater than 5", if the last digit is greater than 5
+if last_digit > 5:
+  print(f"Last digit of {number} is {last_digit} and is greater than 5")
+
+# ..." and is less than 6 and not 0", if the last digit is less than 6 and not 0
+elif last_digit < 6 and last_digit != 0:
+  print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+
+# ..." and is 0", if the last digit is 0
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
