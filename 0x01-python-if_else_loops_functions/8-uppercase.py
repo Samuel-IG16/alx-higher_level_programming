232c1c1983b1b0cce5a4745728ef56e15d53e3f0
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:07:11 +0100

    Task 8 completed!

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print a string in uppercase
+# demonstrates how to use a function and if condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def uppercase(str):
-    for i in str:
-        i = ord(i)
-        if 97 <= i <= 122:
-            i = i - 32
-        print("{:c}".format(i), end='')
+    """Prints uppercase string
+
+    Args:
+        str: a character string argument
+    """
+
+    # For each letter in str
+    for letter in str:
+
+      # Get the letter ascii code
+      ascii_letter_code = ord(letter)
+
+      # If the letter is in the lowercase range
+      if ascii_letter_code in range(97, 123):
+
+        # Set it to the uppercase value
+        ascii_letter_code = ascii_letter_code - 32
+
+      # Print out the uppercase value as character
+      print(f"{ascii_letter_code:c}", end='')
     print()
