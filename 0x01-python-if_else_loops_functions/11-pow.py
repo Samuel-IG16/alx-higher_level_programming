c06215293c1bf2e73e3cfa0b7f4b060d14463749
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:07:51 +0100

    Task 11 completed!

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to compute a to the power of b and return the value
+# demonstrates how to use a function to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 def pow(a, b):
+    """Raises a number to a power
+
+    Args:
+        a: number argument
+        b: power argument
+    
+    Returns:
+        the result of exponentiation
+    """
     return (a ** b)
