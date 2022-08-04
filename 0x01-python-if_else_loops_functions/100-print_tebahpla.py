4bf6f90fb1301f8d5b7c6636279f3b9cd7f1f60f
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-04 23:08:22 +0100

    Task 100 completed!

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
-for ascii_num in range(122, 96, -1):
-    if ascii_num % 2 == 1:
-        ascii_num = ascii_num - 32
-    print("{:c}".format(ascii_num), end='')
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print prints the ASCII alphabet, in reverse order, alternating lowercase and 
+# uppercase (z in lowercase and Y in uppercase)
+# demonstrates how to use a loop and an if...elif condition to affect program output
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Define the loop with lowercase letters starting from last letter
+# first lowercase letter has odd ascii_code
+# last lowercase letter has even ascii_code
+# For odd ascii_codes convert letters to uppercase
+
+# For ascii_code in range sequence
+for ascii_code in range(122, 96, -1):
+
+  # If ascii_code is odd
+  if ascii_code % 2 == 1:
+
+    # Convert letter to uppercase
+    ascii_code = ascii_code - 32
+
+  # Print out letters
+  print(f"{ascii_code:c}", end='')
