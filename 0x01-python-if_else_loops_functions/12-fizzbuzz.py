7bf7060146e03fbe5d7fd6f357ff968f0ad27628
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 14:12:42 +0100

    Task 12 completed!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,36 +2,38 @@
 # -----------------------------------------------------------
 # Python program that:
 # demonstrates how to print the numbers from 1 to 100 separated by a space
-# demonstrates how to use a function, a loop and an if...elif condition to affect program output
+# demonstrates how to use a function, a loop and an if...elif condition to
+# affect program output
 #
 # (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
 # email igbinijesusamuel@gmail.com
 # -----------------------------------------------------------
 
+
 def fizzbuzz():
-  """Print out numbers from 1 to 100"""
+    """Print out numbers from 1 to 100"""
 
-  # For number in range sequence
-  for number in range(1, 101):
+    # For number in range sequence
+    for number in range(1, 101):
 
-    # If number is a multiple of three
-    if number % 3 == 0 and number % 5 != 0:
+        # If number is a multiple of three
+        if number % 3 == 0 and number % 5 != 0:
 
-      # Print out "Fizz "
-      print("Fizz ", end="")
+            # Print out "Fizz "
+            print("Fizz ", end="")
 
-    # If number is a multiple of five but not of three
-    elif number % 5 == 0 and number % 3 != 0:
+        # If number is a multiple of five but not of three
+        elif number % 5 == 0 and number % 3 != 0:
 
-      # Print out "Buzz "
-      print("Buzz ", end="")
+            # Print out "Buzz "
+            print("Buzz ", end="")
 
-    # If number is a multiple of both three and five
-    elif number % 15 == 0:
-      
-      # Print out "FizzBuzz "
-      print("FizzBuzz ", end="")
-  
-    else:
-      # Print number
-      print(f"{number} ", end="")
+        # If number is a multiple of both three and five
+        elif number % 15 == 0:
+
+            # Print out "FizzBuzz "
+            print("FizzBuzz ", end="")
+
+        else:
+            # Print number
+            print(f"{number} ", end="")
