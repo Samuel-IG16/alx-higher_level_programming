f76d7feb9bf1c6c9a8897f35a86497f694bc271e
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:03:58 +0100

    Task 5 completed!

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to import the variable a from the file
+# variable_load_5.py and prints its value.
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import a from file
+from main import a
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
     print(a)
