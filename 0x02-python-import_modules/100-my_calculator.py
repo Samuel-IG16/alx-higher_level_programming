9c51e855877e47b0692708f029b5bbf5c6afc272
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:04:12 +0100

    Task 100 completed!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to import the variable a from the file
+# variable_load_5.py and handles basic operations
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import sys module to access argv
 from sys import argv
-from calculator_1 import add, sub, mul, div
 
-ops = ['+', '-', '*', '/']
-argc = len(argv)
+# Import the functions: add, sub, mul, div
+from main import add, sub, mul, div
 
-if __name__ == '__main__':
-    if (argc - 1) < 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+operators = ["+", "-", "*", "/"]
+arg_len = len(argv)
+
+if __name__ == "__main__":
+    if (arg_len - 1) < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] not in ops:
-        print('Unknown operator. Available operators: +, -, * and /')
+    elif argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
         a, b = int(argv[1]), int(argv[3])
-        if argv[2] == '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif argv[2] == '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif argv[2] == '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        if argv[2] == "+":
+            print(f"{a} + {b} = {add(a, b)}")
+        elif argv[2] == "-":
+            print(f"{a} - {b} = {sub(a, b)}")
+        elif argv[2] == "*":
+            print(f"{a} * {b} = {mul(a, b)}")
         else:
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+            print(f"{a} / {b} = {div(a, b)}")
