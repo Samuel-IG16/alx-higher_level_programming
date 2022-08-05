21091717c8710983fea6d3724c7328165e893ae7
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 14:06:53 +0100

    Task 7 completed!

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -8,16 +8,17 @@
 # email igbinijesusamuel@gmail.com
 # -----------------------------------------------------------
 
+
 def islower(c):
     """Checks for lowercase
 
     Args:
         c: a single character argument
-    
+
     Returns:
         True: if c is lowercase
         False: if c is not lowercase
     """
-    if (ord(c) >= 97 and ord(c) <= 122):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     return False
