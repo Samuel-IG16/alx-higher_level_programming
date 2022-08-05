914a1a12acf17c190a99b9158e09ab148157552b
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 14:05:28 +0100

    Task 6 completed!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -11,13 +11,13 @@
 # Digits are 0 ... 9
 # Loop variables to increment:  first_digit, second_digit
 # For smallest combination first_digit < second_digit
-# For unique combination first_digit != second_digit 
+# For unique combination first_digit != second_digit
 # For smallest unique combination !(second_digit <= first_digit)
 
 for first_digit in range(0, 8):
-  for second_digit in range(0, 10):
-    if not (second_digit <= first_digit):
-      print(f"{first_digit}{second_digit}", end=", ")
+    for second_digit in range(0, 10):
+        if not (second_digit <= first_digit):
+            print(f"{first_digit}{second_digit}", end=", ")
 
 # Print last values for first_digit and second_digit
 print("89")
