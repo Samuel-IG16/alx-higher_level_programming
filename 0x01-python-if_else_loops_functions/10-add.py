82ce947476bde666caf469e41d78a0b6956b543d
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 14:10:31 +0100

    Task 10 completed!

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -8,14 +8,15 @@
 # email igbinijesusamuel@gmail.com
 # -----------------------------------------------------------
 
+
 def add(a, b):
     """Adds two integers
 
     Args:
         a: first integer
         b: second integer
-    
+
     Returns:
         the result of addition
     """
-    return (a + b)
+    return a + b
