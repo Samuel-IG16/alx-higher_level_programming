5e0f75d0e71fcbe2d66bee7d557bee23c42e1f38
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:03:26 +0100

    Task 3 completed!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the result of the addition of all
+# commandline arguments
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import sys module to access argv
 from sys import argv
-summ = 0
+
+# Initialize sum to zero
+sum = 0
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
+
+    # Iterate through the arguments
     for nums in argv:
+
+        # Skip the first argument
         if argv.index(nums) == 0:
             continue
-        summ += int(nums)
-    print(summ)
+        # Sum all other arguments
+        sum += int(nums)
+
+    # Print out sum
+    print(sum)
