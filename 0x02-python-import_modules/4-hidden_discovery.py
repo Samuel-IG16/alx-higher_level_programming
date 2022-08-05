f65bb6c3871ba90ceb3fdc01b3d3c7b2fc66274d
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:03:41 +0100

    Task 4 completed!

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print all the names defined by the compiled module
+# using dir() hidden_4.pyc (please download it locally)
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import hidden_4
+import hidden_4
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
-    import sys
-    import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+
+    # Iterate through list of defined names in module
+    for def_name in dir(hidden_4):
+        if def_name[:2] != "__":
+            print(def_name)
