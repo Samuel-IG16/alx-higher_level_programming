3f53640eb32600f89f4f49aef38bc7334c2d1945
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:04:26 +0100

    Task 101 completed!

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print #pythoniscool, followed by a new line,
+# in the standard output.
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
