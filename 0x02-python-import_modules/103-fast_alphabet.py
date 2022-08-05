428f20a6cfe064dc1cf7aa00c3457746742cdf85
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-05 20:04:52 +0100

    Task 103 completed!

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the alphabet in uppercase, followed by a new line
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import the string module
 import string
+
+# Print out alphabets
 print(string.ascii_uppercase)
