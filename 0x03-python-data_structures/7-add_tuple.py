4ee0a07f74a1d356f4026355377dd57b24ecb93c
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 00:07:28 +0100

    Task 7 completed!

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to use a function to adds 2 tuples
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
+    """Adds two tuples
+
+    Args:
+        tuple_a: first tuple
+        tuple_b: second tuple
+
+    Returns:
+        a tuple with 2 integers:
+    """
+
     if len(tuple_a) == 0:
         tuple_a += (0, 0)
     elif len(tuple_a) == 1:
@@ -13,5 +32,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b += (0,)
     elif len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
-    res = tuple(sum(item) for item in zip(tuple_a, tuple_b))
-    return res
+
+    # parallel iteration in tuple comprehension
+    sum_of_tuple = tuple(sum(item) for item in zip(tuple_a, tuple_b))
+    return sum_of_tuple
