fc9030458adfc8aca8c2b21b7a98853cc23104ce
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-06 17:08:37 +0100

    Task 1 completed!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -16,7 +16,7 @@ b = 5
 
 # This code should not run when this file is imported
 if __name__ == "__main__":
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
