e08f7ee366c4a5bdf829be2ad03a541f911b975b
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-06 16:54:18 +0100

    Task 1 completed!

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to retrieve an element from a list like in C
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def element_at(my_list, idx):
+    """Retrieves an element from a list like in C
+
+    Args:
+        my_list: a list
+        idx: the index of item to retrieve
+
+    Returns:
+        the item at index idx
+    """
+
+    # Check for negative and out of range index
     if idx < 0:
         return None
     if idx >= len(my_list):
