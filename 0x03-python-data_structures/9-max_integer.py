e7557c3b12988e2c2e1683931e00067aa34af342
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 00:07:53 +0100

    Task 9 completed!

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to find the biggest integer of a list
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def max_integer(my_list=[]):
+    """
+    Args:
+        my_list: a list
+
+    Returns:
+        the biggest integer in list or none if list is empty
+    """
+
     if len(my_list) == 0:
-        return "None"
+        return None
     else:
-        max = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max:
-                max = my_list[i]
-        return max
+        biggest_int = my_list[0]
+        for index in range(len(my_list)):
+            if my_list[index] > biggest_int:
+                biggest_int = my_list[index]
+    return biggest_int
