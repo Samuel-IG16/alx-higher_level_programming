d9a3b44847cc2ee951a14c614c6ccbb0a02efca0
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-06 16:54:38 +0100

    Task 2 completed!

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to replace an element from a list at a specific position like in C
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def replace_in_list(my_list, idx, element):
+    """Replaces an element of a list at a specific position
+
+    Args:
+        my_list: a list
+        idx: the index of item to replace
+        element: item to be substituted
+
+    Returns:
+        the edited list
+    """
+
+    # Check for negative and out of range index
     if idx < 0:
         return my_list
     if idx >= len(my_list):
