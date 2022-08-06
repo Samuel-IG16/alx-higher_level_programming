ad96de424980dfbf40f50ad560ec6c637cc69a48
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 00:07:15 +0100

    Task 6 completed!

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print a matrix of integers
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers
+
+    Args:
+        matrix: a matrix
+    """
+
+    # If no argument is supplied print a newline
     if not matrix:
         print()
     else:
-        for row in range(len(matrix)):
-            for item in range(len(matrix[row])):
-                if item != len(matrix[row]) - 1:
-                    endspace = ' '
+        # Loop through the rows
+        for row in matrix:
+            # Loop through the items in row
+            for item in row:
+                # Add space between items if not last item in row
+                if row.index(item) != len(row) - 1:
+                    endspace = " "
                 else:
-                    endspace = ''
-                print("{:d}".format(matrix[row][item]), end=endspace)
+                    endspace = ""
+                # Print the items in the row
+                print("{:d}".format(item), end=endspace)
             print()
