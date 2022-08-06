7fe0d13fbe516af5279f1cd7fb159ab56e9b6aef
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 00:08:07 +0100

    Task 10 completed!

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to find all multiples of 2 in a list
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list
+
+    Args:
+        my_list: a list
+
+    Returns:
+        a new list with True or False
+    """
+
     new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
+    for index in range(len(my_list)):
+        if my_list[index] % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
