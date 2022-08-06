54d064a5e1754097879776a64117a5f175630c46
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-06 20:07:57 +0100

    Task 4 completed!

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to replace an element in a list at a specific position
+# without modifying the original list (like in C)
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def new_in_list(my_list, idx, element):
+    """Replace an element in a list at a specific location
+      without modifying the original
+
+    Args:
+        my_list: a list
+        idx: the index of item to replace
+        element: item to be substituted
+
+    Returns:
+        the edited list copy
+    """
+
     temp_list = my_list.copy()
     if idx < 0:
         return temp_list
