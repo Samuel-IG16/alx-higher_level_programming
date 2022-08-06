4a8aad77f4e81f7c038f1234cac878355ad9cf03
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-06 20:08:12 +0100

    Task 5 completed!

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to removes all characters c and C from a string
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    """Removes all characters c and C from a string
+
+    Args:
+        my_list: a string
+
+    Returns:
+        the new string
+    """
+
+    # Create a mapping table(dictionary) that sets the characters
+    # to their replacement values
+    dict = {ord(char): None for char in "cC"}
+
+    # Call the translate string method giving it the mapping table
+    new_string = my_string.translate(dict)
     return new_string
