cfe8b0204aceb46375fa1d0ac9d33421b8c67cc6
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:11:16 +0100

    Task 4 completed!

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a set of all elements present in only one set
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def only_diff_elements(set_1, set_2):
     return set_1 ^ set_2
