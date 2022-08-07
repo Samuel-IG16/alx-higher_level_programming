f6a82c9801d0807ca9f2d8093994242b4132f765
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:10:26 +0100

    Task 0 completed!

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to compute the square value of all integers of a matrix
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    new_matrix = [[number**2 for number in row] for row in matrix]
     return new_matrix
