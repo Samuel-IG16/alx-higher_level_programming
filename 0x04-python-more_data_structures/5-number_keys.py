e85fceb1e27cf48ade82ff8e7ba44d2b70297743
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:11:28 +0100

    Task 5 completed!

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return the number of keys in a dictionary
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def number_keys(a_dictionary):
     return len(a_dictionary.keys())
