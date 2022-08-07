551b81e1ed3c418e0df41fc126b45bfa8ba78dd1
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:11:05 +0100

    Task 3 completed!

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a set of common elements in two sets
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def common_elements(set_1, set_2):
     return set_1 & set_2
