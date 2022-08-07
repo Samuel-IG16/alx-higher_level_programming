cc7a5cffe6600c91553c0648bea0b69cec2caa0c
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:10:52 +0100

    Task 2 completed!

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to add all unique integers in a list (only once
+# for each integer)
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def uniq_add(my_list=[]):
-    new = set(my_list)
-    res = 0
-    for i in new:
-        res += i
-    return res
+    my_set = set(my_list)
+    sum = 0
+    for number in my_set:
+        sum += number
+    return sum
