4f8cd0e7f3c06f45c22b4d707afd21035c1f51e9
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:11:53 +0100

    Task 7 completed!

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to replace or add key/value in a dictionary
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
         a_dictionary[key] = value
