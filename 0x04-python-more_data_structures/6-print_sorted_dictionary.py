add44c16f89ad6dbd7f4a0ab0fc391b32c827b77
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-07 19:11:39 +0100

    Task 6 completed!

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print a dictionary by ordered keys
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        print("{:s}: {}".format(i, a_dictionary[i]))
+    for key in sorted(a_dictionary):
+        print("{:s}: {}".format(key, a_dictionary[key]))
