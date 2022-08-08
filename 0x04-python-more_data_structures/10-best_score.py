da6c9ad4d103fa5ab6cc4e07ba63cf66862d7dda
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:13:00 +0100

    Task 10 completed!

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a key with the biggest integer value
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
