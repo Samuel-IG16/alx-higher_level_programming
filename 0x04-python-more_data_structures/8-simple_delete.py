124209834947f8f72ad8f212aee773a9c00b3dac
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:12:35 +0100

    Task 8 completed!

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to delete a key in a dictionary
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
