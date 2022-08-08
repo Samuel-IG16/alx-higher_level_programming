c8ec6c414e7abc1d803de9f65303645c1a3f6693
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:13:16 +0100

    Task 11 completed!

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a list with all values multiplied by a number
+# without using any loops
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda i: number * i, my_list))
