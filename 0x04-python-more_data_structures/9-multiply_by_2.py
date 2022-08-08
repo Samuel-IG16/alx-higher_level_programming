8d65938d1aedddcc11c088c59c7dcffed2bdf624
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:12:47 +0100

    Task 9 completed!

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to return a new dictionary with all values multiplied by 2
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def multiply_by_2(a_dictionary):
-    temp = dict(a_dictionary)
-    for key, value in temp.items():
-        temp[key] = value * 2
-    return temp
+    new_dict = a_dictionary.copy()
+    for key, value in new_dict.items():
+        new_dict[key] = value * 2
+    return new_dict
