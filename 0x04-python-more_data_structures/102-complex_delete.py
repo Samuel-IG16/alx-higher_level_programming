5ad64061fc55e7c60bf20b9c102b85c640985c6b
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:20:44 +0100

    Task 102 completed!

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to delete keys with a specific value in a dictionary
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def complex_delete(a_dictionary, value):
     for idx in list(a_dictionary.keys()):
         if a_dictionary[idx] == value:
