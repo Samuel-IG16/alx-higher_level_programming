b8a950b5781dc2f7feb50ecb00ce2920f7e4b1b2
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-08 18:20:26 +0100

    Task 101 completed!

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to compute the square value of all integers of a matrix
+# using map
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def square_matrix_map(matrix=[]):
     return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
