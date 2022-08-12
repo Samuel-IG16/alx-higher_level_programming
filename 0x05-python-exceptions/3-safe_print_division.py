5e59eb0491e62f8dc67cd84a763b57528ebd6c75
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 17:39:14 +0100

    Task 3 completed!

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the result of dividing 2 integers
+# demonstrates how to use a try ... except ... finally for exception handling
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def safe_print_division(a, b):
     try:
         div = a / b
