9c2d4f56f93ae3e864a510688c08a265f0ca11a8
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 17:40:04 +0100

    Task 6 completed!

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a name exception with a message
+# demonstrates how to raise exception
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def raise_exception_msg(message=""):
     raise NameError(message)
