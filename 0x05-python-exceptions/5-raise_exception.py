72d6e94123e56cdd8ffc53ce039ff1e9065927f0
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 17:39:44 +0100

    Task 5 completed!

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a type exception
+# demonstrates how to raise exception
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def raise_exception():
     raise TypeError
