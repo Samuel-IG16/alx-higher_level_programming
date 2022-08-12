843577c47bda30a8a6b8582de5539347d0a03207
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 17:39:28 +0100

    Task 4 completed!

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to divide element by element 2 lists
+# demonstrates how to use a try ... except ... finally for exception handling
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def list_division(my_list_1, my_list_2, list_length):
-    div = []
-    temp_result = 0
-    for i in range(0, list_length):
+    new_list = []
+    result = 0
+    for index in range(0, list_length):
         try:
-            temp_result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[index] / my_list_2[index]
         except TypeError:
-            temp_result = 0
+            result = 0
             print("wrong type")
         except ZeroDivisionError:
-            temp_result = 0
+            result = 0
             print("division by 0")
         except IndexError:
-            temp_result = 0
+            result = 0
             print("out of range")
         finally:
             pass
-        div.append(temp_result)
-    return div
+        new_list.append(result)
+    return new_list
