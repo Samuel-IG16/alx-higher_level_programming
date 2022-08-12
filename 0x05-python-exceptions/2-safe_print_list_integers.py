c417c538740b8202e5c1d309bce6d4749ba5f562
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 01:56:21 +0100

    Task 2 completed!

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the first x elements of a list and only integers
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
+    num_of_elements = 0
+    for index in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
+            print("{:d}".format(my_list[index]), end="")
+            num_of_elements += 1
         except (ValueError, TypeError):
             pass
+
     print()
-    return count
+    return num_of_elements
