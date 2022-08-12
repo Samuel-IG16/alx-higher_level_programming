572f8b7feaf897e4b70436ca5f29d84a3cbf8b25
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-12 01:56:09 +0100

    Task 1 completed!

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print an integer with "{:d}".format()
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except(TypeError, ValueError):
+    except (TypeError, ValueError):
         return False
+
+    return True
