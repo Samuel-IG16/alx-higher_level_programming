089291660603817f1c69ca57dfe906102e5bf20d
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-15 20:10:11 +0100

    Task 5 completed!

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,56 +1,75 @@
 #!/usr/bin/python3
-"""Square module."""
+# -----------------------------------------------------------
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+"""Square Class.
+
+This module contains a class that defines a square.
+
+Usage Example:
+
+    Square = __import__('4-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
 
 class Square:
-    """Defines a square."""
+    """Defines the blueprint of a square.
+
+    Attribute:
+        size: An integer indicating the size of the square object.
+    """
 
     def __init__(self, size=0):
-        """Constructor.
+        """An object constructor method.
 
-        Args:
-            size: length of side of the square.
+        Initiatilizes Square with size.
+
+        Arg:
+            size: A integer representing object size.
+                  Has a default value of 0.
         """
-        if not (isinstance(size, int)):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
     def size(self):
-        """Properties for the length of a sise of a square.
+        """Gets the size private attribute value.
 
-        Raises:
-            TypeError: if size is not an integer.
-            ValueError: If size < 0.
+        Returns:
+            The size private attribute
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ setter function for private attribute size
-            Args:
-                value: value to be set.
-            Returns:
-                nothing.
+        """Sets the size private attribute value.
+
+        Validates the assignment of the size private attribute.
+
+        Arg:
+            value: the value to be set
         """
-        if not (isinstance(value, int)):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Area of the square.
+        """A public object method.
 
         Returns:
-            thee size squared.
+            The current square area
         """
-        return self.__size ** 2
+        return self.__size**2
 
     def my_print(self):
-        """Prints the square"""
+        """Displays the square object with # character"""
+
         for i in range(self.size):
             for j in range(self.size):
                 print("#", end="\n" if j is self.size - 1 and i != j else "")
