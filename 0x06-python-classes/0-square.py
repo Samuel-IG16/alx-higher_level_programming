b0ffd2279cb383d0097a5ac3782073adc24f8c01
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-15 15:21:52 +0100

    Task 1 completed!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-"""Square module."""
+# -----------------------------------------------------------
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+"""Square Class.
+
+This module contains an empty class that defines a square.
+
+Usage Example:
+
+    Square = __import__('0-square').Square
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
 
 class Square:
-    """Defines a square"""
+    """Defines the blueprint of a square"""
+
     pass
