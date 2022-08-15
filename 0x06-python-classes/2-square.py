6c62ff520c7976f4acce93111f39cc015a1b4974
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-15 19:24:14 +0100

    Task 2 completed!

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-"""Square module."""
+# -----------------------------------------------------------
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+"""Square Class.
+
+This module contains a class that defines a square.
+
+Usage Example:
+
+    Square = __import__('2-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
 
 class Square:
-    """Defines a square."""
+    """Defines the blueprint of a square.
+
+    Attribute:
+        size: An integer indicating the size of the square object.
+    """
 
     def __init__(self, size=0):
-        """Constructor.
+        """An object constructor method.
 
-        Args:
-            size: length of side of the square.
+        Initiatilizes Square with size.
 
-        Raises:
-            TypeError: if size is not an integer.
-            ValueError: If size < 0.
+        Arg:
+            size: A integer representing object size.
+                  Has a default value of 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
