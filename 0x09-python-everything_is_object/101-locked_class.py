e431bbeafa39b07e6b4fb5d25a81d79ff2b0e585
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-22 20:52:09 +0100

    Task 101 completed!

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-class LockedClass:
-    """A locked class that only lets the user dynamically create the instance
+"""A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
 
+
+class LockedClass:
     __slots__ = ["first_name"]
 
     def __init__(self, first_name=""):
