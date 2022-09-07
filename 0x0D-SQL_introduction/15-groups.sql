37523e46b2d641d636f5e94ac990670fb8a6e313
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-07 15:14:30 -0700

    Task completed!

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
