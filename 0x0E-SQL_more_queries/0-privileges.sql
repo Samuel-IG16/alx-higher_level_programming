579c80699acbb265fd29955a093c13760c567dd9
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-07 15:32:03 -0700

    Task completed!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
