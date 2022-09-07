a833da540980fcc80e450009781417586c54363b
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-07 15:00:10 -0700

    Task completed!

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 --  inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, Best School);
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
