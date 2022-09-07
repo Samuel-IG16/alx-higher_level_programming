a6d1bd0d7a479a19c964bea92793b10505b55fbf
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-07 15:16:01 -0700

    Task completed!

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
